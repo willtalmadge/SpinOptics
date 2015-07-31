19f63e07c58d702bdac5c94be27c31b7009ca94f
willtalmadge <willtalmadge@gmail.com>
2015-07-31 14:27:05 -0600

    Return an empty string when there is no match

--- spin_optics/data_wrangling/file_identification.py
+++ spin_optics/data_wrangling/file_identification.py
@@ -4,4 +4,8 @@ import re
 def filename_contains_string(string, filename):
     return re.search('.*' + string + '.*', filename) is not None
 def filename_containing_string(string, directory='.'):
-    return [x for x in os.listdir(directory) if filename_contains_string(string, x) == True][0]
+    files = [x for x in os.listdir(directory) if filename_contains_string(string, x) == True]
+    if len(files) > 0:
+        return files[0]
+    else:
+        return ''
