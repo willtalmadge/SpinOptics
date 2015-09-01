38d052c45131068be510285c9be78d05fa7ca07c
willtalmadge <willtalmadge@gmail.com>
2015-09-01 10:42:23 -0600

    Fixed noop lambda for post post processing
    Also removed debug

--- spin_optics/fitting/batch.py
+++ spin_optics/fitting/batch.py
@@ -2,7 +2,7 @@ import scipy.optimize as opt
 
 def progressive_fit(data, loader, init_p, p_names, model,
                     keys, key_name='Timestamp', x_name='Field', y_name='FR', 
-                    post_process=lambda: None):
+                    post_process=lambda a, b, c, d: None):
     """
     Fits a `model` to data files returned by loader, mapped by keys in data. This batch
     procedure fits in the order the keys appear in `keys` using the fitting solution
@@ -23,13 +23,10 @@ def progressive_fit(data, loader, init_p, p_names, model,
     """
     last_p = init_p
     for k in keys:
-        print(k)
         d = loader(k)
         if len(d.index) == 0:
             return
-        print(last_p)
         p, cov = opt.curve_fit(model, d[x_name], d[y_name], last_p, maxfev=10000)
-        print(p)
         for i in range(0, len(p)):
             data.loc[data[key_name] == k, p_names[i]] = p[i]
         last_p = p
