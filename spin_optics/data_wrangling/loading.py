7c7e1481b50c3cb4cb99de450ce2bc42dfa35d86
willtalmadge <willtalmadge@gmail.com>
2015-09-01 10:44:10 -0600

    data_for_value now uses a loader function
    This loader function does not itself need concat ability, it should
    provide the ability to locate all possible files that could be
    interesting.

--- spin_optics/data_wrangling/loading.py
+++ spin_optics/data_wrangling/loading.py
@@ -3,20 +3,21 @@ from pandas import read_csv
 from spin_optics.data_wrangling.file_identification import *
 
 
-def data_for_value(data, value, column_name, key_name='Timestamp'):
+def data_for_value(data, value, column_name, loader, key_name='Timestamp'):
     """
     Given a dataframe with a key column that uniquely identifies additional data files in
     the current directory, load and concatenate those files that share the same value.
     :param data: The dataframe that maps values to keys (keys are usually timestamps)
     :param value: the values to lookup in data (for example a wavelength)
     :param column_name: the column to search for values (for example the wavelength column)
+    :param loader: a function that produces a data frame for the given key_name
     :param key_name: the name of the column that uniquely maps value to an associated measurement data file
     :return: returns a dataframe that concatenates all of the files in the directory that map to `value` via key
     """
-    rs = data[data[column_name] == value]
-    result = read_csv(filename_containing_string(str(int(rs.iloc[0, key_name]))))
-    if rs.count().Timestamp > 1:
-        for i, r in rs[1:].iterrows():
-            d = read_csv(filename_containing_string(str(int(r[key_name]))))
+    matching_rows = data[data[column_name] == value]
+    result = loader(matching_rows.iloc[0, key_name])
+    if len(matching_rows.index) > 1:
+        for i, r in matching_rows[1:].iterrows():
+            d = loader(r[key_name])
             result = result.append(d)
     return result
